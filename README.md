868e3a28afa5fa821de2c6513cbc6111184127a7
Chris Shepherd <chris@loneless.org>
2017-05-18 14:02:19 +0200

    Readability update

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
+# Binary Clock
+
 A clock. In binary. Displaying three 6bit numbers: hours, minutes and seconds.
 
 Demo at [http://loneless.org/clock](http://loneless.org/clock)
+
+JS has got a neat feature in the Number prototype function `#toString`: represent a number in characters. The most famous range of characters would be 0-1 (2, binary), 0-f (16, hexadecimal). 
+
+```Javascript
+Number(200).toString(2);// outputs '11001000'
+Number(200).toString(3); // outputs '21102'
+// ...
+Number(200).toString(16); // outputs 'c8'
+```
+
+That's all that it takes.
+
+```
+001101 => 13
+111010 => 58
+110011 => 51
+
+13:58:51
+```
+
+I chose 6-bit numbers, because it's all that was needed for displaying hour, minute or second.
+
+2^6 = 64
--- cli.js
+++ cli.js
@@ -1,16 +1,15 @@
 /**
  * Fits the length of the input string to the specified length.
  * E.g. Useful to fit a 6bit string (each char either 1 or 0) to an 8bit string
- * @param {String} input The input string
  * @param {Number} length Length of the output string
  * @param {String} filler The char to fill the additional chars with
  * @return {String} Final string with specified length
  */
-function toBit(input, length, filler) {
+String.prototype.toBit = function (length, filler) {
 	filler = filler || "0";
-	let string = "";
-	for (let i = 0; i < length - input.length; i++) string += filler;
-	string += input;
+	var string = "";
+	for (var i = 0; i < length - this.length; i++) string += filler;
+	string += this;
 	return string;
 };
 
@@ -20,8 +19,8 @@ function toBit(input, length, filler) {
 function loop() {
 	const time = new Date();
 	console.log(
-		toBit((time.getHours() >>> 0).toString(2), 6), // calc hours
-		toBit((time.getMinutes() >>> 0).toString(2), 6), // calc minutes
-		toBit((time.getSeconds() >>> 0).toString(2), 6)); // calc seconds
+		(time.getHours() >>> 0).toString(2).toBit(6), // calc hours
+		(time.getMinutes() >>> 0).toString(2).toBit(6), // calc minutes
+		(time.getSeconds() >>> 0).toString(2).toBit(6)); // calc seconds
 }
 setInterval(loop, 1000);
